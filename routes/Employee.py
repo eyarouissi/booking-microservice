78fd8cec01855629a51d21545c2e0fa0ddf5ff96
eyarouissi <63970415+eyarouissi@users.noreply.github.com>
2021-07-27 11:20:35 +0200

    Update Employee.py

--- routes/Employee.py
+++ routes/Employee.py
@@ -17,8 +17,11 @@ router = APIRouter()
 async def get_project_data(project_code: str):
 
     doc = list ( Projects_collection.find({ "project_code" : project_code}))
-    return ResponseModel("the project  is : {}  ".format(doc), "Project retrieved successfully")
     
+    if doc == [] :
+     return ErrorResponseModel("An error occured.", 404, " The project is not found")
+    else :
+     return ResponseModel("the project  is : {}  ".format(doc), "Project retrieved successfully")
 
 #Receives a post request from WIX containing a booked session. 
 @router.post("/receive_booking")
@@ -29,9 +32,19 @@ async def receive_booking( employee : dict):
         print(Employee['project_code'])
         code = Employee['project_code'].strip()
         email = Employee['email'].strip()
-        
-        Projects_collection.update_one({ "project_code" : code, "Employees.email"  : email },
-        {"$set" : {"Employees.$.scheduled_session"  : Employee['start_timestamp'] +" to "+ Employee['end_timestamp']}},True )
+        start_time =Employee['start_timestamp']
+        end_time =Employee['end_timestamp']
+
+        match_start = re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z', start_time)
+        match_end = re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z', end_time)
+        start_date = datetime.strptime(match_start.group(), "%Y-%m-%dT%H:%M:%SZ").isoformat()
+        end_date = datetime.strptime(match_end.group(), "%Y-%m-%dT%H:%M:%SZ").isoformat()
+        doc = list ( Projects_collection.find({ "project_code" : code, "Employees.email"  : email}))
+        if doc==[] :
+            return ErrorResponseModel("An error occured.", 404, " Email is not available in the Employees")
+        else:
+            Projects_collection.update_one({ "project_code" : code, "Employees.email"  : email },
+            {"$set" : {"Employees.$.scheduled_session"  : "From " + start_date +" to "+ end_date}},True )
       
-        return ResponseModel("the data of this employee who booked : {} is updated successfully".format(Employee), "Project updated successfully") \
+            return ResponseModel("the data of this employee who booked : {} is updated successfully".format(Employee), "Project updated successfully") \
         
